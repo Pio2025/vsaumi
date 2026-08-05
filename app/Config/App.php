7fe8955ad199b9f017f8c8a05196e3ed1ffd0b52
John Doe <piobaleicoqe@yahoo.com>
2026-08-06 08:38:43 +1200

    Point baseURL at production domain (vsaumi.com)
    Real DB credentials moved to .env instead of being committed to
    Database.php, keeping them out of git history.

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,8 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/vsaumi/public/';
+    //public string $baseURL = 'http://localhost/vsaumi/public/';
+    public string $baseURL = 'https://vsaumi.com/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
